68269b8fcbe9b0aaa9913efc02ff192f355a4ff0
0xcryptolub <blackhat1523@gmail.com>
2023-04-08 11:15:00 +0530

    random image can be generated from now

--- src/components/Inp.js
+++ src/components/Inp.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM  from "react-dom";
+import ranmeme from "../memesData"
+
+
 
 export default  function(){
+    
+    const [MemeImg, setMemeImg] = React.useState("")
+
+    function randommeme(){
+        const rannum = (Math.floor(Math.random()*ranmeme.data.memes.length))
+        setMemeImg(ranmeme.data.memes[rannum].url)
+    }
+
     return(
         <main>
             <div className="form">
                 <input type="text" placeholder="Top text" className="form--input"/>
                 <input type="text" placeholder="Bottom text" className="form--input"/>
-                <button className="form--btn">Get a new meme image 🖼</button>
+                <button className="form--btn" onClick={randommeme}>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme--img-div">
+                <img src={MemeImg} className="meme--img"/>    
             </div>
         </main>
     )
